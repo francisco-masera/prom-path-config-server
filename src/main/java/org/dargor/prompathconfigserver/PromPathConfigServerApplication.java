f7dc9b8862e068ca6413a4806989895702bf146e
Dargor94 <franciscomasera1994@gmail.com>
2023-06-25 02:10:26 -0300

    git confg uri

--- src/main/java/org/dargor/prompathconfigserver/PromPathConfigServerApplication.java
+++ src/main/java/org/dargor/prompathconfigserver/PromPathConfigServerApplication.java
@@ -2,8 +2,9 @@ package org.dargor.prompathconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@EnableConfigServer
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class PromPathConfigServerApplication {
 
