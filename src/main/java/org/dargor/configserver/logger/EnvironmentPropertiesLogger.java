ad45fc9e3a782cbad416a1480b33b27a57ba3add
Dargor94 <franciscomasera1994@gmail.com>
2023-09-24 00:03:31 -0300

    props logger

---
+++ src/main/java/org/dargor/configserver/logger/EnvironmentPropertiesLogger.java
@@ -0,0 +1,29 @@
+package org.dargor.configserver.logger;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class EnvironmentPropertiesLogger {
+
+    @EventListener
+    public void handleContextRefreshed(ApplicationContextInitializedEvent event) {
+        ConfigurableEnvironment env = event.getApplicationContext().getEnvironment();
+        log.info("{}={}", "bael.property", env.getProperty("bael.property"));
+        log.info("{}={}", "app.name", env.getProperty("app.name"));
+    }
+
+    @EventListener
+    public void handleContextRefreshed(ContextRefreshedEvent event) {
+        ConfigurableEnvironment env = (ConfigurableEnvironment) event.getApplicationContext().getEnvironment();
+        log.info("{}={}", "git-user", env.getProperty("spring.cloud.config.server.git.username"));
+        log.info("{}={}", "git-pwd", env.getProperty("spring.cloud.config.server.git.password"));
+    }
+}
