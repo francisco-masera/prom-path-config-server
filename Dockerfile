8f6e6185849aea0347a48743d75c1477d7bfaed6
Dargor94 <franciscomasera1994@gmail.com>
2023-10-16 17:21:37 -0300

    rollback to ghcr

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM gradle:6.9.1-jdk11-alpine AS tmp_build_img
-WORKDIR /app
-COPY . .
-RUN gradle build
-
 FROM azul/zulu-openjdk-alpine:11.0.20
-COPY --from=tmp_build_img . .
+
+RUN mkdir /app
+
+COPY app.jar /app/app.jar
+
+WORKDIR /app
+
 EXPOSE 8888
-ENTRYPOINT ["java", "-jar", "/app/build/libs/app.jar"]
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/org/dargor/configserver/logger/EnvironmentPropertiesLogger.java
+++ src/main/java/org/dargor/configserver/logger/EnvironmentPropertiesLogger.java
@@ -2,7 +2,6 @@ package org.dargor.configserver.logger;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -14,16 +13,10 @@ import org.springframework.stereotype.Component;
 public class EnvironmentPropertiesLogger {
 
     @EventListener
-    public void handleContextRefreshed(ApplicationContextInitializedEvent event) {
-        ConfigurableEnvironment env = event.getApplicationContext().getEnvironment();
-        log.info("{}={}", "git-user", env.getProperty("spring.cloud.config.server.git.username"));
-        log.info("{}={}", "git-pwd", env.getProperty("spring.cloud.config.server.git.password"));
-    }
-
-    @EventListener
     public void handleContextRefreshed(ContextRefreshedEvent event) {
         ConfigurableEnvironment env = (ConfigurableEnvironment) event.getApplicationContext().getEnvironment();
         log.info("{}={}", "git-user", env.getProperty("spring.cloud.config.server.git.username"));
         log.info("{}={}", "git-pwd", env.getProperty("spring.cloud.config.server.git.password"));
     }
+
 }
